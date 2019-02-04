9d1d97dad86b86507ce10b5ef2485f12ffb74fe9
Viorel Zota <zota.v@mobilesoft.com>
2019-02-04 17:49:53 +0200

    remove firebase

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,7 @@
 package com.example.myapplication;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -19,21 +11,6 @@ public class MainActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		FirebaseInstanceId.getInstance().getInstanceId()
-				.addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-					@Override
-					public void onComplete(@NonNull Task<InstanceIdResult> task) {
-						if (!task.isSuccessful()) {
-							Log.w(TAG, "getInstanceId failed", task.getException());
-							return;
-						}
-
-						// Log and toast
-						String msg =  task.getResult().getToken();
-						Log.d(TAG, msg);
-						Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-					}
-				});
 		
 	}
 }
